5f91bb7f53c30faa38ba6e9259dea238fbe80f7e
serfa <dserfezi@gmail.com>
2022-07-05 17:45:30 +0200

    added_dyn_load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit, ViewChild, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
 import { GlobalsService } from './globals.service';
 import { EventsService } from './events.service';
 import { SerialService, rdKeys_t } from './serial.service';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
+
+import { HTU21D_005_Component } from './htu21d-005/htu21d-005.component';
+import { BME280_007_Component } from './bme280-007/bme280-007.component';
+import { SSR_009_Component } from './ssr-009/ssr-009.component';
+import { Actuator_010_Component } from './actuator-010/actuator-010.component';
+import { DBL_SW_008_Component } from './dbl-sw-008/dbl-sw-008.component';
+import { ZB_Bridge_Component } from './zb-bridge/zb-bridge.component';
 
 const USB_CMD_STATUS_OK = 0x00;
 //const USB_CMD_STATUS_FAIL = 0x01;
@@ -14,6 +21,8 @@ const USB_CMD_STATUS_OK = 0x00;
 })
 export class AppComponent implements OnInit, OnDestroy {
 
+    @ViewChild('dynamic', {read: ViewContainerRef}) private viewRef: ViewContainerRef;
+
     secGroup: FormGroup;
     linkKey: string = 'link-key-1234567';
     epid: string = 'epid-123';
@@ -22,12 +31,14 @@ export class AppComponent implements OnInit, OnDestroy {
     scrollFlag = true;
 
     partNum = 0;
+    prevPartNum = -1;
     startFlag = true;
 
     constructor(public serial: SerialService,
                 public globals: GlobalsService,
                 private events: EventsService,
-                private ngZone: NgZone) {
+                private ngZone: NgZone,
+                private cfr: ComponentFactoryResolver) {
         // ---
     }
 
@@ -81,6 +92,25 @@ export class AppComponent implements OnInit, OnDestroy {
         });
         this.events.subscribe('readPartNumRsp', (msg: number)=>{
             this.partNum = msg;
+            if(this.partNum != this.prevPartNum) {
+                this.prevPartNum = this.partNum;
+                console.log('***---***');
+                this.viewRef.clear();
+                switch(this.partNum) {
+                    case this.globals.ZB_BRIDGE: {
+                        const componentFactory = this.cfr.resolveComponentFactory(ZB_Bridge_Component);
+                        this.viewRef.createComponent(componentFactory);
+                        break;
+                    }
+                    case this.globals.HTU21D_005: {
+                        const componentFactory = this.cfr.resolveComponentFactory(HTU21D_005_Component);
+                        this.viewRef.createComponent(componentFactory);
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
             console.log(`part number: ${this.partNum}`);
             if(this.startFlag == true) {
                 this.startFlag = false;
@@ -92,6 +122,19 @@ export class AppComponent implements OnInit, OnDestroy {
                 }, 200);
             }
         });
+    }
+
+    /***********************************************************************************************
+     * fn          loadPartComponent
+     *
+     * brief
+     *
+     */
+    loadPartComponent(){
+        if(this.partNum != this.prevPartNum){
+
+        }
+
     }
 
     /***********************************************************************************************
